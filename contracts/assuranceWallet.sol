9aa6814bba3ff28a65746bba2f34303482fb34b6
BILALck4 <chekhchou.bilal@etu.uae.ac.ma>
2024-01-21 21:59:48 +0100

    creating new contract and modifying old ones

--- contracts/assuranceWallet.sol
+++ contracts/assuranceWallet.sol
@@ -1,26 +1,85 @@
-
 // SPDX-License-Identifier: MIT 
 pragma solidity ^0.8.0;
-contract  assuranceWallet{
 
-  address public owner;
-  uint256 public myBalance;
-
-  mapping(address => uint256) public wallets;
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/VestingWallet.sol";
 
 
-  event Deposit(address indexed account, uint256 amount);
-  event Withdraw(address indexed account, uint256 amount);
+contract AssuranceWallet is Ownable {
 
-  constructor() {
-    owner = msg.sender;
-    myBalance=msg.sender.balance;
-  }
+    address public owner;
 
-  modifier onlyOwner() {
-    require(msg.sender == owner, "Only the owner can call this function");
-    _;
-  }
+    struct Transaction {
+        uint256 transactionId;
+        address walletSender;
+        address walletReceiver;
+        uint256 amount;
+        uint256 date;
+        bool isApproved;
+        bytes32 signature;
 
- 
+    }
+
+    Transaction[] public transactions;
+
+    mapping(address => uint256) public wallets;
+
+    event Deposit(address indexed account, uint256 amount);
+    event Withdraw(address indexed account, uint256 amount);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
+    modifier walletExists(address _wallet) {
+        require(wallets[_wallet] > 0, "Wallet does not exist");
+        _;
+    }
+
+    modifier sufficientBalance(address _wallet, uint256 _amount) {
+        require(wallets[_wallet] >= _amount, "Insufficient funds");
+        _;
+    }
+
+    constructor() Ownable() {
+        // No need to reinitialize 'owner' and 'myBalance'
+    }
+
+    function changeOwner(address _newOwner) public onlyOwner {
+        owner = _newOwner;
+    }
+
+    function addWallet(address _wallet) public onlyOwner {
+        wallets[_wallet] = 0;
+    }
+
+    function deposit() public payable {
+        wallets[msg.sender] += msg.value;
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function withdraw(uint256 _amount) public walletExists(msg.sender) sufficientBalance(msg.sender, _amount) {
+        wallets[msg.sender] -= _amount;
+        payable(msg.sender).transfer(_amount);
+        emit Withdraw(msg.sender, _amount);
+    }
+
+    function getBalance() public view returns (uint256) {
+        return wallets[msg.sender];
+    }
+
+    function getContractBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function sendEther(address payable _receiver, uint256 _amount) public onlyOwner sufficientBalance(_receiver, _amount) {
+        require(_amount <= address(this).balance, "Insufficient funds");
+        _receiver.transfer(_amount);
+        transactions.push(Transaction(transactions.length, msg.sender, _receiver, _amount, block.timestamp, true));
+    }
+
+    function getTransactions() public view returns (Transaction[] memory) {
+        return transactions;
+    }
 }
